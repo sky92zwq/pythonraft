6957da2f53daf99bf763bb247bf58c991ff524b0
zwq <zwq>
2025-02-14 18:07:40 +0800

    adjust

--- node.py
+++ node.py
@@ -1,13 +1,50 @@
+from asyncio import futures
 from enum import Enum
+import time
+
+import grpc
+
+import raft_pb2
+import raft_pb2_grpc
+import logging
+import argparse
 
 class Role(Enum):
     FOLLOWER = 1
     CANDIDATE = 2
     LEADER = 3
 
-class Node:
+import asyncio
+import random
+from raft_pb2 import RequestVoteRequest, RequestVoteResponse, AppendEntriesRequest, AppendEntriesResponse, LogEntry
+
+class DelayedTrigger:
+    def __init__(self, delay: int, callback):
+        self.delay = delay
+        self.callback = callback
+        self._task = None
+
+    async def trigger(self):
+        try:
+            print(f"Waiting for {self.delay} seconds...")
+            # 如果任务被取消，跳出 sleep
+            await asyncio.sleep(self.delay)
+            await self.callback()
+        except asyncio.CancelledError:
+            print("Task was cancelled before it completed.")
+
+    def start(self):
+        # 启动延时触发器任务
+        self._task = asyncio.create_task(self.trigger())
+
+    def cancel(self):
+        if self._task:
+            self._task.cancel()  # 取消任务
+
+
+
+class Node(raft_pb2_grpc.RaftServicer):
     def __init__(self):
-        self.role = Role.FOLLOWER
         self.currentTerm = 0
         self.votedReceived = 0
         self.votedFor = None
@@ -16,16 +53,31 @@ class Node:
         self.log = []
         self.peers = []
 
-    def set_role(self, role):
+        self.followerTimer = None
+        self.leaderTimer = None
+
+        self.id = None
+        asyncio.create_task(self.set_role(Role.FOLLOWER))
+
+    async def set_role(self, role):
         if isinstance(role, Role):
             self.role = role
+            if self.role == Role.LEADER:
+                await self.heartbeat()
+            elif self.role == Role.CANDIDATE:
+                await self.leaderElection()
+            elif self.role == Role.FOLLOWER:
+                await self.handleHeartbeat()
         else:
-            raise ValueError("Invalid role")
+            raise await ValueError("Invalid role")
 
-    def requestVote(self, request, response):
+    async def requestVote(self, request, context):
+        response = raft_pb2.RequestVoteResponse()
+        logging.info(f"Node {self.id} received vote request {request}")
+
         if request.term < self.currentTerm:
             response.voteGranted = False
-            return
+            return await response
 
         if request.term > self.currentTerm:
             self.currentTerm = request.term
@@ -36,43 +88,74 @@ class Node:
             response.voteGranted = True
         else:
             response.voteGranted = False
+        return await response
 
-    def leaderElection(self):
-        self.set_role(Role.CANDIDATE)
+    async def leaderElection(self):
+        logging.info(f"Node {self.id} starts election")
+
+        time.sleep(random.randint(1, 15))
+        logging.info(f"Node {self.id} starts election end")
         self.currentTerm += 1
         self.votedFor = None
+        logging.info(f"Node {self.id} peers {self.peers}")
         for peer in self.peers:
+            logging.info(f"Node {self.id} peer {peer}")
             request = RequestVoteRequest()
             request.candidateId = self.id
             request.term = self.currentTerm
             request.lastLogIndex = len(self.log) - 1
-            request.lastLogTerm = self.log[-1].term
-            response = RequestVoteResponse()
-            peer.requestVote(request, response)
+            if len(self.log) > 0: 
+                request.lastLogTerm = self.log[-1].term
+            else:
+                request.lastLogTerm = 0
+            logging.info(f"Node {self.id} create vote request {request}")
+
+            response = await peer.requestVote(request)
+            logging.info(f"Node {self.id} received vote response {response}")
             if response.voteGranted:
                 self.votedReceived += 1
                 if self.votedReceived > len(self.peers) / 2:
-                    self.set_role(Role.LEADER)
+                    self.leaderTimer = DelayedTrigger(3, self.heartbeat)
+                    logging.info(f"Node {self.id} becomes leader")
+                    await self.set_role(Role.LEADER)
                     return
                 
-    def heartbeat(self):
+    async def heartbeat(self):
         for peer in self.peers:
             request = AppendEntriesRequest()
             response = AppendEntriesResponse()
-            peer.appendEntries(request, response)
+            response = peer.appendEntries(request)
+        self.leaderTimer.start()
 
+    def handleHeartbeat(self):
+        tmp =self.followerTimer
+        self.followerTimer = DelayedTrigger(5, self.handleHeartbeatTimeout)
+        self.followerTimer.start()
+        if tmp:
+            tmp.cancel()
 
-    def appendEntries(self, request, response):
+    async def handleHeartbeatTimeout(self):
+        logging.info(f"Node {self.id} timeout")
+        await self.set_role(Role.CANDIDATE)
+        
+
+    async def appendEntries(self, request, context):
+        response = AppendEntriesResponse()
         if request.term < self.currentTerm:
             response.success = False
-            return
+            return await response
 
         self.currentTerm = request.term
         self.votedFor = None
+        if request.entries is None and request.leaderId != self.id:
+            response.success = True
+            logging.info(f"Node {self.id} becomes follower")
+            await self.set_role(Role.FOLLOWER)
+            return await response
 
         if len(self.log) < request.prevLogIndex or self.log[request.prevLogIndex].term != request.prevLogTerm:
             response.success = False
-            return
+            return await response
 
         self.log = self.log[:request.prevLogIndex + 1]
         self.log.extend(request.entries)
@@ -81,6 +164,7 @@ class Node:
             self.commitIndex = min(request.leaderCommit, len(self.log) - 1)
 
         response.success = True
+        return await response
 
 
     def write(self, data):
@@ -97,8 +181,9 @@ class Node:
             request.term = self.currentTerm
             request.entries = [entry]
             request.leaderCommit = self.commitIndex
+            request.leaderId = self.id
             response = AppendEntriesResponse()
-            peer.appendEntries(request, response)
+            response = peer.appendEntries(request)
             if response.success:
                 appendCnt += 1
         if appendCnt > len(self.peers) / 2:
@@ -107,12 +192,30 @@ class Node:
         else:
             return False
         return True
-    
-    def stateMachineLoop(self):
-        while True:
-            if self.role == Role.LEADER:
-                self.heartbeat()
-            elif self.role == Role.CANDIDATE:
-                self.leaderElection()
-            else:
-                pass
+async def serve():
+    parser = argparse.ArgumentParser(description="Raft Node")
+    parser.add_argument("--id", type=int, help="Node ID", required=True)
+    parser.add_argument("--peers", type=lambda s: s.split(','), help="Peer addrs", required=True)
+    parser.add_argument("--port", type=int, help="Port", default=5003)
+
+    node = Node()
+    node.id = parser.parse_args().id
+    print("Node started {}".format(parser.parse_args().id))
+    print("Node started {}".format(parser.parse_args().peers))
+    print("Node started {}".format(parser.parse_args().port))
+    for peer in parser.parse_args().peers:
+        peer = grpc.aio.insecure_channel(peer)
+        stub = raft_pb2_grpc.RaftStub(peer)
+        node.peers.append(stub)
+        logging.info(f"Node {node.id} connected to peer {peer}")
+
+    server = grpc.aio.server()
+    raft_pb2_grpc.add_RaftServicer_to_server(node, server)
+    server.add_insecure_port("localhost:"+(str(parser.parse_args().port)))
+    await server.start()
+    logging.info("gRPC Server running on port {}...".format(parser.parse_args().port))
+    await server.wait_for_termination()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    asyncio.run(serve())
