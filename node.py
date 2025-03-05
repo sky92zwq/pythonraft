96b3c2c079eec19031561c1aa600f6b09b66bc36
zwq <zwq>
2025-03-05 17:38:51 +0800

    http, remove tenacity, log

--- node.py
+++ node.py
@@ -3,6 +3,8 @@ from enum import Enum
 import time
 
 import grpc
+import fastapi
+import uvicorn
 
 import raft_pb2
 import raft_pb2_grpc
@@ -26,12 +28,12 @@ class DelayedTrigger:
 
     async def trigger(self):
         try:
-            print(f"Waiting for {self.delay} seconds...")
+            logger.info(f"start a timer Waiting for {self.delay} seconds...")
             # 如果任务被取消，跳出 sleep
             await asyncio.sleep(self.delay)
             await self.callback()
         except asyncio.CancelledError:
-            print("Task was cancelled before it completed.")
+            logger.error("Task was cancelled before it completed.")
 
     def start(self):
         # 启动延时触发器任务
@@ -52,12 +54,21 @@ class Node(raft_pb2_grpc.RaftServicer):
         self.commitIndex = 0
         self.log = []
         self.peers = []
+        self.channels = []
 
         self.followerTimer = None
         self.leaderTimer = None
 
         self.id = None
+        self.role = Role.FOLLOWER
         asyncio.create_task(self.set_role(Role.FOLLOWER))
+
+        self.app = fastapi.FastAPI()
+        self.router = fastapi.APIRouter()
+        
+        self.router.get("/all")(self.getAll)  # 绑定成员函数
+        self.app.include_router(self.router)   # 注册路由
+
 
     async def set_role(self, role):
         if isinstance(role, Role):
@@ -98,6 +109,7 @@ class Node(raft_pb2_grpc.RaftServicer):
         time.sleep(random.randint(5, 15))
         logging.info(f"Node {self.id} starts election end")
         self.currentTerm += 1
+        self.votedReceived = 0
         self.votedFor = None
         logging.info(f"Node {self.id} peers {self.peers}")
         for peer in self.peers:
@@ -112,7 +124,7 @@ class Node(raft_pb2_grpc.RaftServicer):
                 request.lastLogTerm = 0
             logging.info(f"Node {self.id} create vote request {request}")
 
-            response = await peer.RequestVote(request)
+            response = await peer.RequestVote(request, wait_for_ready = True)
             logging.info(f"Node {self.id} received vote response {response}")
             if response.voteGranted:
                 self.votedReceived += 1
@@ -133,7 +145,7 @@ class Node(raft_pb2_grpc.RaftServicer):
         request.leaderCommit = self.commitIndex
         logging.info(f"Node {self.id} sends heartbeat is {request}")
         for peer in self.peers:
-            response = await peer.AppendEntries(request)
+            response = await peer.AppendEntries(request, wait_for_ready = False)
             logging.info(f"Node {self.id} sends heartbeat got {response}")
         self.leaderTimer.start()
 
@@ -214,53 +226,62 @@ class Node(raft_pb2_grpc.RaftServicer):
             return False
         return True
     
+    async def getAll(self):
+        return {"currentTerm": self.currentTerm, "votedReceived": self.votedReceived, "votedFor": self.votedFor, "commitIndex": self.commitIndex, "log": self.log, "role": self.role}
+
+    async def serve(self, info):
+        self.id = info.id
+        logger.info(f"Node {self.id} started {info.port}")
+        server = grpc.aio.server()
+        raft_pb2_grpc.add_RaftServicer_to_server(self, server)
+        server.add_insecure_port("localhost:"+(str(info.port)))
+
+        await server.start()
+        logger.info("Node started {}".format(info.peers))
+
+        for peer in info.peers:
+            peer = grpc.aio.insecure_channel(peer)
+            await channel_ready(peer)
+            self.channels.append(peer)
+            stub = raft_pb2_grpc.RaftStub(peer)
+            self.peers.append(stub)
+            logging.info(f"Node {self.id} connected to peer {peer} and stub is {stub}")
+
+        await server.wait_for_termination()
+
+    # 用协程方式运行 FastAPI
+    async def serve_http(self, info):
+        config = uvicorn.Config(self.app, host="0.0.0.0", port=info.port+3000, loop="asyncio")
+        server = uvicorn.Server(config)
+        await server.serve()
 async def channel_ready(channel):
     try:
         await channel.channel_ready()
     except grpc.RpcError as e:
         logging.error(f"Channel not ready: {e}")
-from tenacity import (
-    retry,
-    wait_exponential,
-    stop_after_attempt,
-    retry_if_exception_type,
-    before_sleep_log
-)
+
 
 logger = logging.getLogger(__name__)
-
-@retry(
-    wait=wait_exponential(multiplier=1, max=2),  # 指数退避，最大间隔10秒
-    stop=stop_after_attempt(5),                   # 最多重试5次
-    retry=retry_if_exception_type(grpc.RpcError), # 仅重试 gRPC 错误
-    before_sleep=before_sleep_log(logger, logging.WARNING)  # 重试前打印日志
-)
-
-async def serve():
+class ParseInfo:
+    def __init__(self):
+        self.id = None
+        self.peers = None
+        self.port = None
+# 运行 gRPC + HTTP（FastAPI）
+async def main(info):
+    node = Node()
+    await asyncio.gather(  # 并行运行 gRPC 和 HTTP
+        node.serve(info),
+        node.serve_http(info)
+    )
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.DEBUG)
     parser = argparse.ArgumentParser(description="Raft Node")
     parser.add_argument("--id", type=int, help="Node ID", required=True)
     parser.add_argument("--peers", type=lambda s: s.split(','), help="Peer addrs", required=True)
     parser.add_argument("--port", type=int, help="Port", default=5003)
-
-    node = Node()
-    node.id = parser.parse_args().id
-    print("Node started {}".format(parser.parse_args().id))
-    server = grpc.aio.server()
-    raft_pb2_grpc.add_RaftServicer_to_server(node, server)
-    server.add_insecure_port("localhost:"+(str(parser.parse_args().port)))
-    logging.info("gRPC Server running on port {}...".format(parser.parse_args().port))
-    await server.start()
-    print("Node started {}".format(parser.parse_args().peers))
-    print("Node started {}".format(parser.parse_args().port))
-    for peer in parser.parse_args().peers:
-        peer = grpc.aio.insecure_channel(peer)
-        await channel_ready(peer)
-        stub = raft_pb2_grpc.RaftStub(peer)
-        node.peers.append(stub)
-        logging.info(f"Node {node.id} connected to peer {peer} and stub is {stub}")
-
-    await server.wait_for_termination()
-
-if __name__ == "__main__":
-    logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.DEBUG)
-    asyncio.run(serve())
+    info = ParseInfo()
+    info.id = parser.parse_args().id
+    info.peers = parser.parse_args().peers
+    info.port = parser.parse_args().port
+    asyncio.run(main(info))
